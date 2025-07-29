6f903cafb986fd0e5a7475d925eb9fd413f107f9
pasqualedimaro <pakydm15@gmail.com>
2025-07-29 12:01:07 +0200

    add global context and initial layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,40 @@
-import { useState } from 'react'
+import { useContext, useState } from 'react'
+import GlobalContext, {GlobalProvider} from "./components/GlobalContext";
 
-function App() {
+const AppContent = () => {
+  const {movie} = useContext(GlobalContext);
 
   return (
     <>
-      <div>ciao</div>
+      <div className='container'>
+        <h2>Risultati Ricerca</h2>
+        {movie.length > 0 ? (
+          movie.map((movie) =>(
+            <div className = "col" key={movie.id}>
+              <ul>
+                <div>
+                  <li className='border border-danger'>
+                    <p className='m-2'>{movie.title}</p>
+                    <p className='m-2'>{movie.original_title}</p>
+                    <p className='m-2'> {movie.original_language}</p>
+                    <p className='m-2'>{movie.vote_average}</p>
+                  </li>
+                </div>
+              </ul>
+            </div>
+          ))
+        ) : (
+          <p>Nessun Risulteto</p>
+        )}
+      </div>
     </>
   )
 }
 
-export default App
+const App = () => (
+  <GlobalProvider>
+    <AppContent />
+  </GlobalProvider>
+)
+
+export default App;
---
+++ src/components/GlobalContext.jsx
@@ -0,0 +1,24 @@
+import { createContext, useState, useEffect} from 'react';
+
+const GlobalContext = createContext()
+
+export const GlobalProvider = ({children}) => {
+    const [movie, setMovie] = useState([]);
+
+    const fetchData = (url = 'https://api.themoviedb.org/3/search/movie?api_key=d5793350e75d5ca4e75bd4fb2759e1ee&query=matrix+reloaded') =>
+    fetch(url)
+     .then((res) => res.json())
+     .then((data) =>{
+        setMovie(data.results)
+    })
+}
+
+useEffect(() => {
+    fetchData();
+}, [])
+
+return (
+    <GlobalContext.Provider value={{movie, fetchData}}>
+        {children}
+    </GlobalContext.Provider>
+)
